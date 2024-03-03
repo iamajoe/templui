0ef4f59e0921f28a40d29aa7085d0cfbbdfa623d
Joel Santos <joe@joesantos.io>
2024-03-03 14:02:09 +0000

    setup tests for new argument opts

--- button/button_test.go
+++ button/button_test.go
@@ -9,33 +9,59 @@ import (
 )
 
 func TestButton(t *testing.T) {
-	comp := New().
-		WithKind(KindSubmit).
-		WithDisabled().
-		WithClasses("foo").
-		WithFunction(func(p *Button) error {
-			p.ClassNames = append(p.ClassNames, "bar")
-			return nil
-		})
-
-	r, w := io.Pipe()
-	go func() {
-		comp.Render(context.Background(), w)
-		w.Close()
-	}()
-
-	doc, err := goquery.NewDocumentFromReader(r)
-	if err != nil {
-		t.Fatalf("failed to read template: %v", err)
+	cases := [](struct {
+		comp         func() *Button
+		assertTarget string
+	}){
+		{
+			comp: func() *Button {
+				return New().
+					WithKind(KindSubmit).
+					WithDisabled().
+					WithClasses("foo").
+					WithFunction(func(p *Button) error {
+						p.ClassNames = append(p.ClassNames, "bar")
+						return nil
+					})
+			},
+			assertTarget: `button.foo.bar[type="submit"][disabled]`,
+		}, {
+			comp: func() *Button {
+				return New(
+					WithKind(KindSubmit),
+					WithDisabled(),
+					WithClasses("foo"),
+					func(p *Button) error {
+						p.ClassNames = append(p.ClassNames, "bar")
+						return nil
+					},
+				)
+			},
+			assertTarget: `button.foo.bar[type="submit"][disabled]`,
+		},
 	}
 
-	docHTML, err := doc.Html()
-	if err != nil {
-		t.Fatalf("failed to return html: %v", err)
-	}
+	for _, test := range cases {
+		comp := test.comp()
 
-	target := `button.foo.bar[type="submit"][disabled]`
-	if doc.Find(target).Length() == 0 {
-		t.Fatalf("expected button to render with attributes: %s", docHTML)
+		r, w := io.Pipe()
+		go func() {
+			comp.Render(context.Background(), w)
+			w.Close()
+		}()
+
+		doc, err := goquery.NewDocumentFromReader(r)
+		if err != nil {
+			t.Fatalf("failed to read template: %v", err)
+		}
+
+		docHTML, err := doc.Html()
+		if err != nil {
+			t.Fatalf("failed to return html: %v", err)
+		}
+
+		if doc.Find(test.assertTarget).Length() == 0 {
+			t.Fatalf("expected button to render with attributes: %s", docHTML)
+		}
 	}
 }
