f665cfc5a35c8ac463e00edd863b3df373d3b488
Joel Santos <joe@joesantos.io>
2024-03-12 11:53:24 +0000

    fix documentation for table usage

--- table/styleguide_templ.go
+++ table/styleguide_templ.go
@@ -30,10 +30,10 @@ var styleguideItems = []styleguideItem{
   table.WithID("zing"),
   table.WithClasses("border border-slate-200"),
   table.WithAttributes(map[string]any{"data-zed": "zung"}),
-  WithHeader("ID", "Status"),
-  WithHeaderClasses("bg-slate-50 border-b border-slate-800"),
-  WithRows([]string{"1", "<span class=\"text-red-500\">Rejected</span>"}),
-  WithRowClasses("border-b border-slate-800"),
+  table.WithHeader("ID", "Status"),
+  table.WithHeaderClasses("bg-slate-50 border-b border-slate-800"),
+  table.WithRows([]string{"1", "<span class=\"text-red-500\">Rejected</span>"}),
+  table.WithRowClasses("border-b border-slate-800"),
 ) 
     `),
 		opts: []OptFn{
